8a8801974262629fb0c977fed2f3c0d966e68861
lazrobadzo <144172921+lazrobadzo@users.noreply.github.com>
2023-11-02 09:22:20 -0700

    Add files via upload

---
+++ hugee.py
@@ -0,0 +1,26 @@
+#x =[2, 4, 6, 2, 5, 2, 7 ]
+
+#for i in range (2) :
+
+    #x.remove (5)
+    
+    #print(x)
+
+
+
+
+
+
+
+
+def   samkutxedis_gverdebi (  A , B , C ) :
+  if ( A+B )   <   c  :
+
+    print ("ასეთი  სამკუთხედი არ იქნება")
+
+  elif ( A , B ) >  c :
+
+    print ("ასეთი  სამკუთხედი იქნება")
+
+
+    samkutxedis_katetebi  (  5 , 6 , 9 )
